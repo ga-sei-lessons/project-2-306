ddf9183471d40b8be68c7b05dd498b6f152df591
weston-bailey <wdbailey87@gmail.com>
2023-04-06 12:28:44 -0700

    adds server and leaves route pseudocode

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+// required packages
+const express = require('express')
+const router = express.Router()
+
+// GET /users/new -- show route for a form that creates a new user (sign up for the app)
+
+// POST /users -- CREATE a new user from the form @ GET /users/new
+
+// GET /users/login -- show route for a form that lets a user login
+
+// POST /users/login -- authenticate a user's credentials
+
+// GET /users/logout -- log out the current user
+
+// GET /users/profile -- show authorized users their profile page
+
+// export the router instance
+module.exports = router
